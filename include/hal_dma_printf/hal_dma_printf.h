bf317fa9f0990ba981be286f5790c01d6be9cdab
Yuta Fujiyama <yuta.technology@gmail.com>
2025-12-30 08:14:44 +0900

    Refactor UART handler setup and add echo functionality

--- include/hal_dma_printf/hal_dma_printf.h
+++ include/hal_dma_printf/hal_dma_printf.h
@@ -1,13 +1,18 @@
 #ifndef HAL_DMA_PRINTF_H
 #define HAL_DMA_PRINTF_H
 
+#include <stdbool.h>
+
 #include "usart.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void HalDmaPrintfSetUartHandler(UART_HandleTypeDef* huart);
+void HalDmaPrintfSetup(UART_HandleTypeDef* huart,
+                                bool enable_echo);
+void HalDmaPrintfEnableEcho();
+void HalDmaPrintfDisableEcho();
 
 #ifdef __cplusplus
 }  // extern "C"
--- src/hal_dma_printf.cc
+++ src/hal_dma_printf.cc
@@ -1,5 +1,6 @@
 #include "hal_dma_printf/hal_dma_printf.h"
 
+#include <cstdio>
 #include <cstring>
 
 #include "usart.h"
@@ -11,25 +12,27 @@
 namespace hal_dma_printf {
 
 UART_HandleTypeDef* huart_ = nullptr;
-char tx_buffer_[HAL_DMA_PRINTF_BUFFER_SIZE];
-size_t read_itr_;
-size_t write_itr_;
+uint8_t tx_buffer_[HAL_DMA_PRINTF_BUFFER_SIZE];
+uint8_t rx_buffer_[HAL_DMA_PRINTF_BUFFER_SIZE];
+size_t tx_read_itr_;
+size_t tx_write_itr_;
+size_t rx_read_itr_;
+bool enable_echo_ = false;
 
 void StartDmaTransmit() {
-  if (write_itr_ < read_itr_) {
-    size_t first_part_size = HAL_DMA_PRINTF_BUFFER_SIZE - read_itr_;
-    HAL_UART_Transmit_DMA(huart_, (uint8_t*)&tx_buffer_[read_itr_],
-                          first_part_size);
-    read_itr_ = 0;
+  if (tx_write_itr_ < tx_read_itr_) {
+    size_t first_part_size = HAL_DMA_PRINTF_BUFFER_SIZE - tx_read_itr_;
+    HAL_UART_Transmit_DMA(huart_, &tx_buffer_[tx_read_itr_], first_part_size);
+    tx_read_itr_ = 0;
   } else {
-    HAL_UART_Transmit_DMA(huart_, (uint8_t*)&tx_buffer_[read_itr_],
-                          write_itr_ - read_itr_);
-    read_itr_ = write_itr_;
+    HAL_UART_Transmit_DMA(huart_, &tx_buffer_[tx_read_itr_],
+                          tx_write_itr_ - tx_read_itr_);
+    tx_read_itr_ = tx_write_itr_;
   }
 }
 
 void OnDmaTransmitComplete([[maybe_unused]] UART_HandleTypeDef* huart) {
-  if (read_itr_ != write_itr_) { StartDmaTransmit(); }
+  if (tx_read_itr_ != tx_write_itr_) { StartDmaTransmit(); }
 }
 
 void SetUartHandler(UART_HandleTypeDef* huart) {
@@ -47,42 +50,86 @@ void SetUartHandler(UART_HandleTypeDef* huart) {
         "[HalDmaPrintf] Error: TX DMA not initialized for UART.\r\n";
     HAL_UART_Transmit(huart_, message, sizeof(message), 100);
     return;
+  } else if (huart->hdmarx == nullptr) {
+    uint8_t message[] =
+        "[HalDmaPrintf] Error: RX DMA not initialized for UART.\r\n";
+    HAL_UART_Transmit(huart_, message, sizeof(message), 100);
+    return;
   }
 
   huart_ = huart;
-  read_itr_ = 0;
-  write_itr_ = 0;
+  tx_read_itr_ = 0;
+  tx_write_itr_ = 0;
+  rx_read_itr_ = 0;
 
   huart_->TxCpltCallback = OnDmaTransmitComplete;
   huart_->AbortTransmitCpltCallback = OnDmaTransmitComplete;
+  HAL_UART_Receive_DMA(huart_, (uint8_t*)rx_buffer_,
+                       HAL_DMA_PRINTF_BUFFER_SIZE);
 }
 
 }  // namespace hal_dma_printf
 
-extern "C" void HalDmaPrintfSetUartHandler(UART_HandleTypeDef* huart) {
+extern "C" void HalDmaPrintfSetup(UART_HandleTypeDef* huart, bool enable_echo) {
   hal_dma_printf::SetUartHandler(huart);
+  setvbuf(stdin, NULL, _IONBF, 0);
+  setvbuf(stdout, NULL, _IONBF, 0);
+  hal_dma_printf::enable_echo_ = enable_echo;
+}
+
+extern "C" void HalDmaPrintfEnableEcho() {
+  hal_dma_printf::enable_echo_ = true;
+}
+
+extern "C" void HalDmaPrintfDisableEcho() {
+  hal_dma_printf::enable_echo_ = false;
 }
 
 extern "C" int _write([[maybe_unused]] int file, char* ptr, int len) {
   using namespace hal_dma_printf;
   if (huart_ == nullptr) { return len; }
 
-  if (HAL_DMA_PRINTF_BUFFER_SIZE - write_itr_ >= static_cast<size_t>(len)) {
-    memcpy(&tx_buffer_[write_itr_], ptr, len);
-    write_itr_ += len;
+  if (HAL_DMA_PRINTF_BUFFER_SIZE - tx_write_itr_ >= static_cast<size_t>(len)) {
+    memcpy(&tx_buffer_[tx_write_itr_], ptr, len);
+    tx_write_itr_ += len;
   } else {
-    size_t remaining_space = HAL_DMA_PRINTF_BUFFER_SIZE - write_itr_;
-    memcpy(&tx_buffer_[write_itr_], ptr, remaining_space);
+    size_t remaining_space = HAL_DMA_PRINTF_BUFFER_SIZE - tx_write_itr_;
+    memcpy(&tx_buffer_[tx_write_itr_], ptr, remaining_space);
     size_t overflow_len = len - remaining_space;
     if (overflow_len > HAL_DMA_PRINTF_BUFFER_SIZE) {
       overflow_len = HAL_DMA_PRINTF_BUFFER_SIZE;
     }
     memcpy(tx_buffer_, ptr + remaining_space, overflow_len);
-    write_itr_ = overflow_len;
+    tx_write_itr_ = overflow_len;
   }
-  write_itr_ %= HAL_DMA_PRINTF_BUFFER_SIZE;
+  tx_write_itr_ %= HAL_DMA_PRINTF_BUFFER_SIZE;
 
   if (huart_->gState == HAL_UART_STATE_READY) { StartDmaTransmit(); }
 
   return len;
+}
+
+extern "C" int _read([[maybe_unused]] int file, char* ptr, int len) {
+  using namespace hal_dma_printf;
+  if (huart_ == nullptr) { return len; }
+
+  int rx_counter = 0;
+  while (rx_counter < len) {
+    if (HAL_DMA_PRINTF_BUFFER_SIZE - huart_->hdmarx->Instance->NDTR !=
+        rx_read_itr_) {
+      ptr[rx_counter] = rx_buffer_[rx_read_itr_];
+      ++rx_read_itr_;
+      rx_read_itr_ %= HAL_DMA_PRINTF_BUFFER_SIZE;
+
+      if ((ptr[rx_counter] == '\n' || ptr[rx_counter] == '\r')) {
+        ptr[rx_counter] = '\n';
+        if (enable_echo_) { _write(1, &ptr[rx_counter], 1); }
+        return ++rx_counter;
+      }
+
+      if (enable_echo_) { _write(1, &ptr[rx_counter], 1); }
+      ++rx_counter;
+    }
+  }
+  return rx_counter;
 }
